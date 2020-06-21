b39cc4241a00338d78ddba242351691c8275905a
Lucas Mazo <lucasmazo32@gmail.com>
2020-06-21 12:08:20 -0500

    added style for the random music player on the main screen

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -3,11 +3,14 @@ import Nav from '../components/Nav';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import featureTrack from '../helpers/featureTrack';
+import { ReactComponent as Headphones } from '../assets/images/headphones.svg';
+import { ReactComponent as Play } from '../assets/images/play.svg';
 import music from '../assets/images/music.jpg';
 import random from '../assets/images/random.jpg';
 import actions from '../actions/index';
 import '../assets/style/Main.css';
 import DiscoverBlock from '../components/DiscoverBlock';
+import { CircularProgress } from '@material-ui/core';
 
 const { setTrack } = actions;
 
@@ -16,11 +19,31 @@ function Main({ code, track, setTrack }) {
     featureTrack(code, setTrack);
   }, [setTrack, code])
 
-  console.log(track);
+  const trackName = (name) => {
+    if (name.length > 28) {
+      return name.slice(0, 28) + '...'
+    }
+    return name;
+  }
 
   return (
     <div className="Main">
       <Nav />
+      <button className="btn player">
+        <Headphones className="headphones" />
+        { track ? 
+        <div className="player-info">
+          <span>{ trackName(track.name) }</span>
+          <div className="bars">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+        </div> : <CircularProgress /> }
+        <Play className="play" />
+      </button>
       <DiscoverBlock image={music} name="Trending" />
       <DiscoverBlock image={random} name="Random" />
     </div>
