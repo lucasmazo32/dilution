c4c26ef5802ae004ca7e28b6defdb49b8956f660
Lucas Mazo <lucasmazo32@gmail.com>
2020-06-23 10:17:26 -0500

    added back button when the user is not on the main screen

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import { ReactComponent as Logo } from '../assets/images/discover.svg';
 import { ReactComponent as Back } from '../assets/images/back.svg';
 import '../assets/style/Nav.css';
 
 export default function Nav() {
+  const [backClass, setBackClass] = useState('');
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setBackClass('closed');
+    } else {
+      setBackClass('');
+    }
+  }, [location.pathname])
+
+  const handleClick = () => {
+    history.push('/');
+  };
+
   return (
     <nav className="Nav">
-      <button className="btn back">
+      <button onClick={handleClick} className={`btn back ${backClass}`}>
         <Back />
       </button>
       <Logo className="discover" />
